937ce4b632dd36606603b2694286450bedbccaef
[Evan Paul Gilmore] <sculptoid11@gmail.com>
2023-05-21 23:29:57 -0400

    Added some files but pushing so I can work on my main computer

---
+++ controllers/homeroutes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const { Sequelize } = require('sequelize');
+const { post, User } = require('../models');
+
+
+
+router.get('/', async (req, res) => {
+    try {
+        const post = await post.findAll({
+            include: [
+                {
+                  model: User,
+                  as: 'user', 
+                  attributes: ['email'] 
+                }
+              ]
+            });
+        const posts = post.map((project) => project.get({ plain: true }));
+        const items = cpus.concat(gpus, posts);
+        res.render('homepage', {
+            items
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+
+    }
+});
+
+
